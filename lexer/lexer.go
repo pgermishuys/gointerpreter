6e40b81c4a4d9311707f21d86c4779ff2129f4cd
Pieter Germishuys <pieter@pieterg.com>
2018-08-29 21:47:31 +0200

    Extend Lexer and test with simple Monkey

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,23 +1,79 @@
 package lexer
 
+import (
+	"github.com/pgermishuys/gointerpreter/token"
+)
+
 type Lexer struct {
 	input        string
 	position     int  //current position in input
-	readPosition int  //current position being read (after current character)
+	nextPosition int  //current position being read (after current character)
 	ch           byte //current character under examination
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
 func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
+	if l.nextPosition >= len(l.input) {
 		l.ch = 0 //ASCII for NUL
 	} else {
-		l.ch = l.input[l.readPosition]
+		l.ch = l.input[l.nextPosition]
 	}
-	l.position = l.readPosition
-	l.readPosition += 1
+	l.position = l.nextPosition
+	l.nextPosition += 1
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
 }
